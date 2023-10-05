d10d75701532df265e52c6094ead1c1cb411183c
Phelipe nunes <phelipesenac@gmail.com>
2023-10-05 09:20:58 -0300

    ler um fabricante

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -45,6 +45,21 @@ final class Fabricante {
     
     }
 
+    function lerUmFabricante():array {
+        $sql = "SELECT * FROM fabricantes WHERE id = :id";
+    
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao carregar: ".$erro->getMessage());
+        }
+    
+        return $resultado;
+    } 
+
 
     public function getId(): int {
         return $this->id;
